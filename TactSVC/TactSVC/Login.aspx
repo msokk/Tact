537974a123de092a80949014b00622b7625b320e
Martin Lensment <mlensment@gmail.com>
2011-04-16 15:01:24 +0000

    Algne autentimine

--- TactSVC/TactSVC/Login.aspx
+++ TactSVC/TactSVC/Login.aspx
@@ -9,7 +9,14 @@
 <body>
     <form id="form1" runat="server">
     <div>
-    
+
+
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" onclick="Logi_sisse" Text="Button" />
+        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+
+
     </div>
     </form>
 </body>
--- TactSVC/TactSVC/Login.aspx.cs
+++ TactSVC/TactSVC/Login.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;
 
 namespace TactSVC
 {
@@ -11,7 +12,27 @@ namespace TactSVC
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if ((Session["Logitud"] != null) && (Convert.ToBoolean(Session["Logitud"]) == true))
+            {
+                // If User is Authenticated then moved to a main page
+                if (User.Identity.IsAuthenticated)
+                {
+                    Response.Redirect("test.aspx");
+                    //TextBox3.Text = "autenditud";
+                }
+                else
+                {
+                    TextBox3.Text = "pole autenditud";
+                }
+            }
+        }
 
+        protected void Logi_sisse(object sender, EventArgs e)
+        {
+            //kasutaja autentimine
+            Session["Logitud"] = true;
+            Session["Kasutaja"] = "kasutaja_objekt_siia";
+            Response.Redirect("test.aspx");
         }
     }
 }
